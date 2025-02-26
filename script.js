af0146171568c815f13c63910ac5c61f8eb20f36
Guilherme Olimpio Rodrigues <guizinholimpio1408@outlook.com>
2025-02-26 19:09:41 -0300

    favicon is been added

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const toggleTheme = () => {
   const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/squirtle-de-cria.jpg");
+    img.setAttribute("src", "assets/squirtle-de-cria.jpg");
     img.setAttribute("alt", "Squirtle de óculos");
   } else {
-    img.setAttribute("src", "./assets/squirtle-fofo.jpg");
+    img.setAttribute("src", "assets/squirtle-fofo.jpg");
     img.setAttribute("alt", "Squirtle colhendo flores");
   }
 };
